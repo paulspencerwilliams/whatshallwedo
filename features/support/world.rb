43325761ec613895b34a9770ba2304bf577c32d6
Paul Williams <paul@paulswilliams.me.uk>
2013-08-12 20:13:38 +0100

    removed deprecation warning for find_or_create_by_name dynamic method

--- features/support/world.rb
+++ features/support/world.rb
@@ -2,7 +2,7 @@ module ActivitiesHelper
   def create_activity(name, weather)
     FactoryGirl.create(:activity, 
                       {:name => name, 
-                       :weather => Weather.find_or_create_by_name(weather)})
+                       :weather => Weather.find_or_create_by(name: weather)})
 
   end
 end
