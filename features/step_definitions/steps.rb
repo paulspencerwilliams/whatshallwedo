fc61ab65e429e3605a0bc6e8788a5970374f8ef0
Paul Williams <paul@paulswilliams.me.uk>
2013-08-14 20:13:19 +0100

    slight refactoring

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,5 +1,4 @@
 Given(/^the following set of activites exist$/) do |table|
-
   table.hashes.each do |attrs|
     create_activity(attrs[:activity], attrs[:weather])
   end
@@ -13,5 +12,4 @@ Then(/^I will be presented with 10 random suggestions$/) do
   within_table('list_of_suggestions') do
     should have_xpath("//tbody//tr", :count => 10)
   end
-
 end
--- features/support/world.rb
+++ features/support/world.rb
@@ -3,9 +3,7 @@ module ActivitiesHelper
     FactoryGirl.create(:activity, 
                       {:name => name, 
                        :weather => Weather.find_or_create_by(name: weather)})
-
   end
 end
-
 
 World(ActivitiesHelper)
